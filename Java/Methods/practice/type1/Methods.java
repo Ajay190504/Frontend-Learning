b81d48276b32af08965862d0c8f44099beb4f004
Ajay Dattatraya Waghmare <159612410+Ajay190504@users.noreply.github.com>
2026-01-21 09:46:39 +0530

    Non-Parametrized and non-return typed methods Practice

---
+++ Java/Methods/practice/type1/Methods.java
@@ -0,0 +1,17 @@
+package practice.type1;
+
+//Type 1: Methods without parameters and without return type
+public class Methods {
+    
+    // Method to greet a user("Ajay")
+    public void greetUser() {
+        System.out.println("Hello  Ajay! Welcome to Java programming.");
+    }
+
+    //method to print Square of a number(5)
+    public int square() {
+        int number = 5;
+        System.out.println("Calculating square of " + number+":"+ (number * number));
+    }
+
+}
