2da7b9652b2182ffabe1be13821a258b3f40ae32
Ajay Dattatraya Waghmare <159612410+Ajay190504@users.noreply.github.com>
2026-07-01 13:55:10 +0530

    React counter App using useState Hook

--- React/React Practice/counterapp/src/Counter.jsx
+++ React/React Practice/counterapp/src/Counter.jsx
@@ -1,44 +1,67 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-const counter = () => {
+const Counter = () => {
+
    let [count, setCount] = useState(0);
+   let conCount = 0;
 
-   function increaseCount(){
+   function increaseCount() {
+      conCount=conCount+1;
       setCount(count + 1);
-   }
-   function decreaseCount(){
-      if(count<=0){
-         alert("Count cannot be less than 0");
-      }
-      else{
-      setCount(count - 1);
-      }
-   }
-   function resetCount(){
-      setCount(0);
-   }
-  return (
-    <div>
-      <h1>Welcome To Counter App</h1>
-      <button onClick={increaseCount}>
-         Increase
-      </button>
-      <br />
-      <br />
-      <button onClick={decreaseCount}>
-         Decrease
-      </button>
-      <br />
-      <br />
-      <button onClick={resetCount}>
-         Reset
-      </button>
-      <br />
-      <br />
-      <h2>Count: {count}</h2>
-    </div>
-  )
-}
 
-export default counter;
+      console.log("Concrete Count:", conCount);
+      console.log("State Count:", count);
+   }
+
+   function decreaseCount() {
+      if (count <= 0) {
+         alert("Count cannot be less than 0");
+      } else {
+         conCount=conCount-1;
+         setCount(count - 1);
+
+         console.log("Concrete Count:", conCount);
+         console.log("State Count:", count);
+      }
+   }
+
+   function resetCount() {
+      conCount = 0;
+      setCount(0);
+
+      console.log("Concrete Count:", conCount);
+      console.log("State Count:", count);
+   }
+
+   return (
+      <div>
+         <h1>Welcome To Counter App</h1>
+
+         <button onClick={increaseCount}>
+            Increase
+         </button>
+
+         <br />
+         <br />
+
+         <button onClick={decreaseCount}>
+            Decrease
+         </button>
+
+         <br />
+         <br />
+
+         <button onClick={resetCount}>
+            Reset
+         </button>
+
+         <br />
+         <br />
+
+         <h2>Count: {count}</h2>
+      </div>
+   );
+};
+
+export default Counter;
