a200f6acbe0ba51fc51255aa5757a61b6e2a1fef
Ajay Dattatraya Waghmare <159612410+Ajay190504@users.noreply.github.com>
2026-07-02 14:04:16 +0530

    React 10 Tasks

--- React/React Practice/mychangeapp/src/main.jsx
+++ React/React Practice/mychangeapp/src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
-import StudentProf from './StudentProf.jsx'
-import LiveCharCounter from './LiveCharCounter.jsx'
-import Sum from './Sum.jsx'
-import FavColorPrev from './FavColorPrev.jsx'
-import Counter from './Counter.jsx'
-import CricketScore from './CricketScore.jsx'
-import VolumeCtrl from './VolumeCtrl.jsx'
-import MoodChanger from './MoodChanger.jsx'
-import PasswordLengthChecker from './PasswordLengthChecker.jsx'
-import MiniIDCard from './MiniIDCard.jsx'
+import Sum from './components/Sum.jsx'
+import StudentProf from './components/StudentProf.jsx'
+import LiveCharCounter from './components/LiveCharCounter.jsx'
+import FavColorPrev from './components/FavColorPrev.jsx'
+import Counter from './components/Counter.jsx'
+import CricketScore from './components/CricketScore.jsx'
+import VolumeCtrl from './components/VolumeCtrl.jsx'
+import MoodChanger from './components/MoodChanger.jsx'
+import PasswordLengthChecker from './components/PasswordLengthChecker.jsx'
+import MiniIDCard from './components/MiniIDCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
